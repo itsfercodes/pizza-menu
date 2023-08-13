897979b51bf88a4cb52f6c72f9ae4c7ec001bc9b
Fernando <itsfercodes@outlook.com>
2023-08-13 13:45:27 -0600

    Add starting resources and created first component

---
+++ src/Pizza.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pizza = () => (
+  <div>
+    <img src="pizzas/focaccia.jpg" alt="Pizza focaccia" />
+    <h2>Focaccia</h2>
+    <p>Bread with italian olive oil and rosemary</p>
+  </div>
+);
+
+export default Pizza;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Pizza from './Pizza';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello React!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
   </React.StrictMode>
 );
